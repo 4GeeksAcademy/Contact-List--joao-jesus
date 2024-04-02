69ba858ea65400571a15be5797812b49b5a1bdda
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-04-02 23:01:58 +0000

    updating

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -7,28 +7,49 @@ import { Context } from "../store/appContext";
 export const Contact = () => {
     const { store, actions } = useContext(Context);
     const params = useParams();
-       
+    const handleClick =() =>{
+        
+    }
+    const handleChange =(event) =>{
+        actions(store.Contact.name)
+    }
     return (
        <div className='container'>
             <div className="form-group">
                 <label htmlFor="exampleInputName">Full Name</label>
-                <input type="text" className="form-control" id="InputFullName" aria-describedby="" />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your data with anyone else.</small>
+                <input type="text" 
+                className="form-control" 
+                onChange={handlechange} 
+                id="InputFullName" 
+                aria-describedby="" />
+                <small id="emailHelp" 
+                className="form-text text-muted">We'll never share your data with anyone else.</small>
                 <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label> 
-             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <label htmlFor="exampleInputEmail1">Email address</label> 
+                <input type="email" 
+                className="form-control" 
+                id="exampleInputEmail1" 
+                aria-describedby="emailHelp" />
              
-             <label className="phoneNumber form-check-label" htmlFor="adress">Phone number</label>
-             <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="PhoneNumber" />
+                <label className="phoneNumber form-check-label" 
+                htmlFor="adress">Phone number</label>
+                <input type="number" 
+                className="form-control" 
+                id="exampleInputEmail1" 
+                aria-describedby="PhoneNumber" />
              
-             <label className="address form-check-label" htmlFor="adress">Adress</label>
-             <input type="text" className="form-control" id="adress" aria-describedby="address" />
-             </div>
+                <label className="address form-check-label" 
+                htmlFor="adress">Adress</label>
+                <input type="text" 
+                className="form-control" 
+                id="adress" 
+                aria-describedby="address" />
+                </div>
            
     
             </div>
            
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary" onclick={actions.addContactList}>Submit</button>
             <Link to="/Home" className="manageContacts">Go to Home page</Link>
         </div>
     );
